478baf337418fdc6726dffade80335eff1f40637
pablo11-dev <131197454+pablo11-dev@users.noreply.github.com>
2023-11-16 15:31:31 -0300

    Create comentado.py

---
+++ comentado.py
@@ -0,0 +1,13 @@
+# axios.het("http://localhost:5000/").then(responde) => {
+#     console.log(response.data);
+# });
+
+# function exobrir() {
+#     const lista = document.getElementById("to-do-list");
+#     data.map((item) => {
+#         const li = document.createElement("li");
+#         li.innerHTML = item.ID + " - " + item.Tarefa ;
+#         lista.appendChild(li);
+    
+#     });
+# }
